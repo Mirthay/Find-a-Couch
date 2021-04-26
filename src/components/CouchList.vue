b61f814f758504655eae271424c9de4b473ca0e1
Mirthay <mario991@gmail.com>
2021-04-26 23:58:37 +0200

    CouchList

---
+++ src/components/CouchList.vue
@@ -0,0 +1,19 @@
+<template>
+  <section></section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+section {
+  border: 1px solid;
+  border-radius: 25px;
+  opacity: 0.5;
+  box-shadow: 1px 1px black;
+}
+h2 {
+  text-align: center;
+}
+</style>
