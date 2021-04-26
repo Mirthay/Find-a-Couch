21e545cacdc91cbd0bbd5f8d0ca34443aac2f1de
Mirthay <mario991@gmail.com>
2021-04-26 16:04:58 +0200

    vytvorenie routes v router.js

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -11,3 +11,15 @@ export default {
   props: ['title']
 };
 </script>
+
+<style scoped>
+section {
+  border: 1px solid;
+  border-radius: 25px;
+  opacity: 0.5;
+  box-shadow: 1px 1px black;
+}
+h2 {
+  text-align: center;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,11 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import App from './App.vue';
+/* import App from './App.vue'; */
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', redirect: '/home' },
-    { path: '/home', component: App }
+    { path: '/', redirect: '/couches' },
+    { path: '/couches', component: null },
+    {
+      path: '/couches/:id',
+      component: null,
+      children: [{ path: 'contact', component: null }]
+    }, // couches/c1/contact
+    { path: '/register', component: null },
+    { path: '/request', component: null },
+    { path: '/:notFound(.*)', component: null }
   ]
 });
 
