9a27ab0d8deae7bf6fc3fa0f4da762116611cee8
Mirthay <mario991@gmail.com>
2021-05-10 23:53:00 +0200

    index store linknute modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
 import { createStore } from 'vuex';
 
+import CouchesModule from './modules/Couches/index.js';
+import RequestModule from './modules/Couches/index.js';
+
 const store = createStore({
+  modules: [CouchesModule, RequestModule],
   state() {
     return {
       mutations: {},
