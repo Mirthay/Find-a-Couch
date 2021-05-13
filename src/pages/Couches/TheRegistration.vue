ceabfc358ef1598e3cd243c0f3e47741b5a86b60
Mirthay <mario991@gmail.com>
2021-05-13 15:04:11 +0200

    TheRegistration filled with form

--- src/pages/Couches/TheRegistration.vue
+++ src/pages/Couches/TheRegistration.vue
@@ -1,7 +1,23 @@
 <template>
-  <section>Register</section>
+  <section>
+    <base-card>
+      <h2>Register as a couch now!</h2>
+      <couch-form @save-data="saveData"></couch-form>
+    </base-card>
+  </section>
 </template>
 
 <script>
-export default {};
+import CouchForm from '../../components/couches/CouchForm.vue';
+export default {
+  components: { CouchForm },
+  methods: {
+    saveData(data) {
+      this.$store.dispatch('couches/registerCouch', data);
+      this.$router.replace('/couches');
+    }
+  }
+};
 </script>
+
+<style scoped></style>
