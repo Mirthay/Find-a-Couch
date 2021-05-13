0091e86b82e960fe5d09339c16ef336bcf160b21
Mirthay <mario991@gmail.com>
2021-05-14 01:25:44 +0200

    request action added

--- src/store/modules/Request/actions.js
+++ src/store/modules/Request/actions.js
@@ -1,1 +1,11 @@
-export default {};
+export default {
+  contactCouch(context, payload) {
+    const newRequest = {
+      id: new Date().toISOString(),
+      couchId: payload.couchId,
+      userEmail: payload.email,
+      userMessage: payload.message
+    };
+    context.commit('addRequest', newRequest);
+  }
+};
