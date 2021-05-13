acfbae4505fb406c93f2edea743b1bb4bd542e2e
Mirthay <mario991@gmail.com>
2021-05-13 15:04:29 +0200

    actions added

--- src/store/modules/Couches/actions.js
+++ src/store/modules/Couches/actions.js
@@ -1,1 +1,13 @@
-export default {};
+export default {
+  registerCouch(context, data) {
+    const regData = {
+      id: 'c3',
+      firstName: data.firstName,
+      lastName: data.lastName,
+      areas: data.areas,
+      description: data.description,
+      hourlyRate: data.rate
+    };
+    context.commit('registerCouch', regData);
+  }
+};
