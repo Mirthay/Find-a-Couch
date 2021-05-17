8c7f0be10ecb6b70cf64910da342b3e83d48b2c9
Mirthay <mario991@gmail.com>
2021-05-17 15:08:33 +0200

    filtrovanie requestov podla couchID

--- src/store/modules/Request/getters.js
+++ src/store/modules/Request/getters.js
@@ -1,8 +1,14 @@
 export default {
-  requests(state) {
-    return state.requests;
+  requests(state, _, _2, rootGetters) {
+    const couchId = rootGetters.couchId;
+    return state.requests.filter(req => req.couchId === couchId);
   },
-  noRequests(state) {
-    return state.requests && state.requests.length > 0;
+  noRequests(state, _, _2, rootGetters) {
+    const couchId = rootGetters.couchId;
+    const couchReq = state.requests.filter(req => req.couchId === couchId);
+    return couchReq && couchReq.length > 0;
   }
+  /*  noRequests(_,getters){      //lepsi zapis 
+    return getters.requests && getters.requests.length > 0;
+  } */
 };
