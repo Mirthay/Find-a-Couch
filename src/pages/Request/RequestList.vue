974f1c8b5f19ba0db77707da88abe2a173d4a5e4
Mirthay <mario991@gmail.com>
2021-05-17 14:21:29 +0200

    request list created

--- src/pages/Request/RequestList.vue
+++ src/pages/Request/RequestList.vue
@@ -1,5 +1,55 @@
 <template>
-  <div>
-    Request form
-  </div>
+  <section>
+    <base-card
+      ><header>
+        <h2>Request recived</h2>
+      </header>
+      <ul v-if="noRequests">
+        <request-item
+          v-for="req in requests"
+          :key="req.id"
+          :message="req.userMessage"
+          :email="req.userEmail"
+        >
+        </request-item>
+      </ul>
+      <h3 v-else>You haven't recivet any request yet!</h3>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import RequestItem from '../../components/requests/RequestItem.vue';
+
+export default {
+  components: { RequestItem },
+  data() {
+    return {};
+  },
+  computed: {
+    requests() {
+      return this.$store.getters['requests/requests'];
+    },
+    noRequests() {
+      return this.$store.getters['requests/noRequests'];
+    }
+  }
+};
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
