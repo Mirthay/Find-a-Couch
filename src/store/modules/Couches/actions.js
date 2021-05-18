ca2c62e6e17729c071b457b0adf0e17eae71ab8f
Mirthay <mario991@gmail.com>
2021-05-18 23:20:19 +0200

    error handler added

--- src/store/modules/Couches/actions.js
+++ src/store/modules/Couches/actions.js
@@ -17,10 +17,11 @@ export default {
       }
     );
 
-    //const responseData = await response.json();
+    const responseData = await response.json();
 
     if (!response.ok) {
-      // error
+      const error = new Error(responseData.message || 'Failed to Fatch');
+      throw error;
     }
 
     context.commit('registerCouch', {
@@ -30,7 +31,7 @@ export default {
   },
   async loadCouches(context) {
     const response = await fetch(
-      `https://test-couch-3659d-default-rtdb.firebaseio.com/couches.json`
+      `https://test-couch-3659d-default-rtdb.firebaseio.com/couches.jso`
     );
     const responseData = await response.json();
     if (!response.ok) {
