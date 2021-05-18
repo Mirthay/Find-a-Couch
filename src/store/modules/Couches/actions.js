a76ebc12e58c3654e43a119f1e92106d0d165f9b
Mirthay <mario991@gmail.com>
2021-05-18 18:42:26 +0200

    http request

--- src/store/modules/Couches/actions.js
+++ src/store/modules/Couches/actions.js
@@ -1,13 +1,55 @@
 export default {
-  registerCouch(context, data) {
+  async registerCouch(context, data) {
+    const userId = context.rootGetters.couchId;
     const regData = {
-      id: context.rootGetters.couchId,
       firstName: data.firstName,
       lastName: data.lastName,
       areas: data.areas,
       description: data.description,
       hourlyRate: data.rate
     };
-    context.commit('registerCouch', regData);
+
+    const response = await fetch(
+      `https://test-couch-3659d-default-rtdb.firebaseio.com/couches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(regData)
+      }
+    );
+
+    //const responseData = await response.json();
+
+    if (!response.ok) {
+      // error
+    }
+
+    context.commit('registerCouch', {
+      ...regData,
+      id: userId
+    });
+  },
+  async loadCouches(context) {
+    const response = await fetch(
+      `https://test-couch-3659d-default-rtdb.firebaseio.com/couches.json`
+    );
+    const responseData = await response.json();
+    if (!response.ok) {
+      //error
+    }
+
+    const couches = [];
+
+    for (const key in responseData) {
+      const couch = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        areas: responseData[key].areas,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate
+      };
+      couches.push(couch);
+    }
+    context.commit('setCouches', couches);
   }
 };
