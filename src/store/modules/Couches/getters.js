3698f891f99f5b75f5a403994db7106f5e000ffb
Mirthay <mario991@gmail.com>
2021-05-19 22:23:46 +0200

    once per minute fetching getter

--- src/store/modules/Couches/getters.js
+++ src/store/modules/Couches/getters.js
@@ -9,5 +9,13 @@ export default {
     const couches = getters.couches;
     const couchId = rootGetters.couchId;
     return couches.some(couch => couch.id === couchId);
+  },
+  shouldUpdate(state) {
+    const lastFetch = state.lastFetch;
+    if (!lastFetch) {
+      return true;
+    }
+    const currentTimestamp = new Date().getTime();
+    return (currentTimestamp - lastFetch) / 1000 > 60;
   }
 };
