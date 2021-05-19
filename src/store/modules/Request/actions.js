3ac855951e30db1dfd1db4a52e4a6913cec5c54f
Mirthay <mario991@gmail.com>
2021-05-19 22:24:44 +0200

    fetching requests action

--- src/store/modules/Request/actions.js
+++ src/store/modules/Request/actions.js
@@ -1,11 +1,46 @@
 export default {
-  contactCouch(context, payload) {
+  async contactCouch(context, payload) {
     const newRequest = {
-      id: new Date().toISOString(),
       couchId: payload.couchId,
       userEmail: payload.email,
       userMessage: payload.message
     };
-    context.commit('addRequest', newRequest);
+
+    const response = await fetch(
+      `https://test-couch-3659d-default-rtdb.firebaseio.com/requests/.json`,
+      {
+        method: 'POST',
+        body: JSON.stringify(newRequest)
+      }
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to Fatch');
+      throw error;
+    }
+  },
+  async loadRequests(context) {
+    const response = await fetch(
+      `https://test-couch-3659d-default-rtdb.firebaseio.com/requests.json`
+    );
+    const responseData = await response.json();
+    if (!response.ok) {
+      //error
+    }
+
+    const requests = [];
+    for (const key in responseData) {
+      const request = {
+        id: key,
+        couchId: responseData[key].couchId,
+        userEmail: responseData[key].userEmail,
+        userMessage: responseData[key].userMessage
+      };
+      console.log(request);
+      requests.push(request);
+    }
+    context.commit('setRequests', requests);
   }
 };
