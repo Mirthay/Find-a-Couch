231e7c31a61f45fc60e8edca9be884a62aaf3800
Mirthay <mario991@gmail.com>
2021-05-25 22:59:12 +0200

    userAuth route added

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import NotFound from './components/NotFound.vue';
 import RequestList from './pages/Request/RequestList.vue';
 import ContactCouch from './pages/Request/ContactCouch.vue';
 import TheRegistration from './pages/Couches/TheRegistration.vue';
+import UserAuth from './pages/auth/UserAuth.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -19,6 +20,7 @@ const router = createRouter({
     }, // couches/c1/contact
     { path: '/register', component: TheRegistration },
     { path: '/request', component: RequestList },
+    { path: '/Auth', component: UserAuth },
     { path: '/:notFound(.*)', component: NotFound }
   ]
 });
