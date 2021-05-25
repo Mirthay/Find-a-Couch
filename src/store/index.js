f2a479e236d7e77f1b5ebd9a16676bcce12f620d
Mirthay <mario991@gmail.com>
2021-05-25 23:06:19 +0200

    authModuel imported to root

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,14 @@ import { createStore } from 'vuex';
 
 import CouchesModule from './modules/Couches/index.js';
 import RequestModule from './modules/Request/index.js';
+import AuthModule from './modules/auth/index.js';
 
 const store = createStore({
-  modules: { couches: CouchesModule, requests: RequestModule },
+  modules: {
+    couches: CouchesModule,
+    requests: RequestModule,
+    auth: AuthModule
+  },
   state() {
     return {
       couchId: 'c3'
