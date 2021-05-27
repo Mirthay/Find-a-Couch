eb4a18e2986809799ac71c853cdac5ccd3970539
Mirthay <mario991@gmail.com>
2021-05-27 03:13:55 +0200

    login added to auth

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,5 +1,31 @@
 export default {
-  login() {},
+  async login(context, payload) {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBnF6Bf78fzehU7ZC_Sr7rirHqctjd4YL4',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true
+        })
+      }
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(responseData.message || 'failed to authenticate');
+      throw error;
+    }
+    console.log(responseData);
+
+    context.commit('setUser', {
+      token: responseData.idToken,
+      couchId: responseData.localId,
+      tokenExpiration: responseData.expiresIn
+    });
+  },
   async signup(context, payload) {
     const response = await fetch(
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBnF6Bf78fzehU7ZC_Sr7rirHqctjd4YL4',
