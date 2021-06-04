c2dbe50da807ac30549755c6821d76faf10b4715
Mirthay <mario991@gmail.com>
2021-06-04 15:37:12 +0200

    fixed requests by couchId

--- src/store/modules/Request/getters.js
+++ src/store/modules/Request/getters.js
@@ -1,10 +1,11 @@
 export default {
-  requests(state, _, _2, rootGetters) {
-    const couchId = rootGetters.couchId;
+  requests(state, _, _2, getters) {
+    const couchId = getters['auth/couchId'];
     return state.requests.filter(req => req.couchId === couchId);
   },
-  noRequests(state, _, _2, rootGetters) {
-    const couchId = rootGetters.couchId;
+  noRequests(state, _, _2, getters) {
+    const couchId = getters['auth/couchId'];
+    console.log(couchId);
     const couchReq = state.requests.filter(req => req.couchId === couchId);
     return couchReq && couchReq.length > 0;
   }
