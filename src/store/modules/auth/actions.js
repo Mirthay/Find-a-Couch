6b25fde16a3c00b9c1411cd1a88a380f3f9695e6
Mirthay <mario991@gmail.com>
2021-06-04 14:24:31 +0200

    set timer action to autologout

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,3 +1,4 @@
+let timer;
 export default {
   async login(context, payload) {
     return context.dispatch('auth', { ...payload, mode: 'login' });
@@ -27,34 +28,56 @@ export default {
     if (!response.ok) {
       const error = new Error(responseData.message || 'failed to authenticate');
       throw error;
-    }
+    } // plus to convert to number and *1000 to make number in miliseconds
+    /* const expiresIn = 5000; */ const expiresIn =
+      +responseData.expiresIn * 1000;
+    const expirationDate = new Date().getTime() + expiresIn;
 
     localStorage.setItem('token', responseData.idToken);
     localStorage.setItem('couchId', responseData.localId);
+    localStorage.setItem('tokenExporation', expirationDate);
 
+    timer = setTimeout(function() {
+      context.dispatch('autoLogout');
+    }, expiresIn);
     context.commit('setUser', {
       token: responseData.idToken,
-      couchId: responseData.localId,
-      tokenExpiration: responseData.expiresIn
+      couchId: responseData.localId
     });
   },
   autoLogin(context) {
     const token = localStorage.getItem('token');
     const couchId = localStorage.getItem('couchId');
+    const tokenExporation = localStorage.getItem('tokenExporation');
+
+    const expiresIn = +tokenExporation - new Date().getTime();
+    if (expiresIn < 0) {
+      return;
+    }
+    timer = setTimeout(function() {
+      context.dispatch('autoLogout');
+    }, expiresIn);
 
     if (token && couchId) {
       context.commit('setUser', {
         token: token,
-        couchId: couchId,
-        tokenExpiration: null
+        couchId: couchId
       });
     }
   },
   logout(context) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('couchId');
+    localStorage.removeItem('tokenExporation');
+
+    clearTimeout(timer);
     context.commit('setUser', {
       token: null,
-      couchId: null,
-      tokenExpiration: null
+      couchId: null
     });
+  },
+  autoLogout(context) {
+    context.dispatch('logout');
+    context.commit('didLogout');
   }
 };
